ab8a1f1b538f238b611381d6824491664c63c4b3
Sergey Lesikov <sergey_lesikov@mail.ru>
2022-02-06 18:04:18 +0300

    completed task Lesson2

---
+++ Lesson2Project1/Lesson2Project1Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson2Project1
+{
+    public class Lesson2Project1Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Lesson2Project1Startup>();
+                });
+    }
+}
